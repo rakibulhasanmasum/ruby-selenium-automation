fe38323fd8bf7dcf7ef69d9ad62b36b6af630a33
R_Hasan <sohandoabernahin1@gmail.com>
2022-10-23 11:57:01 +0600

    crawling on trip creation

--- bidding_price_range_trip_test.rb
+++ bidding_price_range_trip_test.rb
@@ -10,4 +10,44 @@ service = Selenium::WebDriver::Service.chrome(path: './chromedriver')
 driver = Selenium::WebDriver.for :chrome, service: service, capabilities: options
 
 driver.get(API_REQUESTS)
+
+
+original_window = driver.window_handle
+
+el = driver.find_element(:id, "create_new_trip")
+el.click
+
+el = driver.find_element(:name, "search_shipper")
+el.send_keys "01819616939"
+
+el = driver.find_element(:id, "search_button")
+el.click
+
+sleep 1
+
+el = driver.find_element(:id, "proceed")
+el.click
+
+sleep 1
+
+# driver.get("https://n.trucklagbe.com/adev/trip_creation?shipperId=378526")
+
+driver.window_handles.each do |handle|
+    if handle != original_window
+        driver.switch_to.window handle
+        break
+    end
+end
+
+el = driver.find_element(:xpath, "//input[@placeholder='Search Load Location']")
+el.send_keys "maniknagar"
+
+sleep 1
+
+el = driver.find_element(:class, "suggestion")
+
+p el
+
+sleep 300
+
 sleep 300
